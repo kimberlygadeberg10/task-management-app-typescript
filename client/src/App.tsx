2baae8198f5aa872c1cb45f7f774abe1acef236e
Kimberly Gadeberg <kimberlygadeberg10@gmail.com>
2026-03-30 18:12:35 -0500

    Added React Router setup with Task Details page and routing structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import TaskDashboard from "./components/TaskDashboard";
+import TaskDetails from "./pages/TaskDetails";
 
 function App() {
   return (
     <div>
       <h1>Task Management App</h1>
-      <TaskDashboard />
+
+      <Routes>
+        {/* Dashboard (home page) */}
+        <Route path="/" element={<TaskDashboard />} />
+
+        {/* Task details page */}
+        <Route path="/task/:id" element={<TaskDetails />} />
+      </Routes>
     </div>
   );
 }
